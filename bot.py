ca4056db854e3f112ab85faceeb3caef70040610
YusufSonmezz <58042458+YusufSonmezz@users.noreply.github.com>
2021-05-17 09:02:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ InstagramtoFacebook.click()
 driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[1]/input")\
     .send_keys("your e-mail")
 driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[2]/input")\
-    .send_keys("yout password")
+    .send_keys("your password")
 driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[3]/button")\
     .click()
 sleep(10)
